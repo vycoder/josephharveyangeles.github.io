41addb1ebd934d79ee566341d17d915951037494
Nestly Cruz <josephharveyangeles@gmail.com>
2017-06-24 14:26:22 +0800

    Updated to 2.0 - Transferred actual site to docs directory - updated dev env to angular4 - updated angular-cli to 1.1.3

---
+++ README.md
@@ -0,0 +1,4 @@
+# Portfolio
+
+This is my personal portfolio website hosted on [link](https://josephharveyangeles.github.io)
+
---
+++ src/app/projects/projectlist/project-item.ts
@@ -0,0 +1,9 @@
+export interface ProjectItem {
+    name: string;
+    description: string;
+    image: string;
+    lang: string[];
+    status: string;
+    repo?: string;
+    awards?: string[];
+}
